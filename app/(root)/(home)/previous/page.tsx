be3c9004e775642e594edd8e41331a465d978ea3
satvik8373 <satvikpatel8373@gmail.com>
2025-02-25 15:14:03 +0530

    s

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,21 +1,7 @@
-"use client";
-
-import React, { useState } from 'react';
 import PreviousMeetings from '@/components/PreviousMeetings';
-import { useUser } from '@clerk/nextjs';
-import { parseISO, isValid, differenceInSeconds, format, differenceInMinutes } from 'date-fns';
-import { cn } from '@/lib/utils';
 
-const PreviousMeetingsPage = () => {
-  const { user } = useUser();
-  const [currentPage, setCurrentPage] = useState(1);
-  const [selectedMeeting, setSelectedMeeting] = useState<MeetingHistory | null>(null);
-
-  return (
-    <div>
-      <PreviousMeetings />
-    </div>
-  );
+const PreviousPage = () => {
+  return <PreviousMeetings />;
 };
 
-export default PreviousMeetingsPage;
+export default PreviousPage;
